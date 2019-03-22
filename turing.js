315371d2dfd58b713efe1b26c8382b5be82d4df0
clemesu1 <clemesu1@unb.ca>
2019-03-22 13:55:44 -0300

    Got tape and input basics working

---
+++ turing.js
@@ -0,0 +1,41 @@
+function drawTape() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    // Top Line
+    ctx.moveTo(0,70);
+    ctx.lineTo(800,70);
+    // Bottom Line
+    ctx.moveTo(0,130);
+    ctx.lineTo(800,130);
+    // Vertical Lines
+    var x = 0
+    for(var i=0; x<=c.width; i++) {
+        ctx.moveTo(x,70);
+        ctx.lineTo(x,130);
+        x += 50
+    }
+
+    ctx.stroke();
+}
+drawTape();
+
+
+function processInput() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "30px Arial";
+    var x = document.getElementById("myText").value;
+    var xstart = 0;
+    var xend = 50;
+    if(xstart != 400)
+    for(var i=0; i<x.length; i++) {
+        var char = x.charAt(i);
+        ctx.fillText(char, ((xstart+xend)/2)-8, 110);
+        xstart = xend;
+        xend += 50;
+    } 
+    ctx.stroke();
+    
+   
+}

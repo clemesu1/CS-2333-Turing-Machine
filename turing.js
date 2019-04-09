6fb6ae4ec640fb7a6214035b1b3dbdb32c2ac442
clemesu1 <43153081+clemesu1@users.noreply.github.com>
2019-04-09 14:30:47 -0300

    Changed font type

--- turing.js
+++ turing.js
@@ -279,7 +279,3 @@ function setCharAt(str,index,chr) {
     if(index > str.length-1) return str;
     return str.substr(0,index) + chr + str.substr(index+1);
 }
-
-function isEmpty(str) {
-    return (!str || 0 === str.length);
-}
